155841d927b63c71cec04821e355b9594ff907e6
wilsonangga <angcungsen@gmail.com>
2020-12-06 09:55:08 +0700

    Testing update via android studio

---
+++ app/src/main/java/com/example/kurditing/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kurditing
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
