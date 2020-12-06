d737958b83a5fb3d5c1f77d9962feeaecead2d25
wilsonangga <angcungsen@gmail.com>
2020-12-06 09:55:41 +0700

    Testing update via android studio

--- app/src/main/java/com/example/kurditing/HomeActivity.kt
+++ app/src/main/java/com/example/kurditing/HomeActivity.kt
@@ -2,8 +2,6 @@ package com.example.kurditing
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
